2499343ac7b077d5b8181857a81a0769d2f83068
FireWolfy89 <dmate899@gmail.com>
2024-10-07 20:45:10 +0200

    Spam protection tested

--- websocket-server/server.js
+++ websocket-server/server.js
@@ -4,13 +4,22 @@ const WebSocket = require('ws');
 
 const server = new WebSocket.Server({ port: 8080 });
 
+const MESSAGE_LIMIT = 5;
+const TIME_WINDOW = 5000;
+
+//Connecting to WS
 server.on('connection', (ws) => {
   console.log('New client has connected to the server');
   ws.isAlive = true;
 
+//WS healthcheck
   ws.on('pong', () => {
     ws.isAlive = true;
   });
+
+  ws.messageCount = 0;
+  ws.startTime = Date.now();
+
 
   const interval = setInterval(() => {
     server.clients.forEach((ws) => {
@@ -21,7 +30,26 @@ server.on('connection', (ws) => {
     });
   }, 30000);
   
+  //Sending the message to all clients + spam protection. If the message limit(5) has been exceeded, the client will be disconnected
   ws.on('message', (message) => {
+
+    const currentTime = Date.now();
+    if(currentTime - ws.startTime < TIME_WINDOW){
+      ws.messageCount++;
+    } 
+    else
+    {
+      ws.messageCount = 1;
+      ws.startTime = currentTime;
+    }
+
+    if(ws.messageCount > MESSAGE_LIMIT)
+    {
+      console.log('Client disconnected due to spamming');
+      ws.close();
+      return;
+    }
+
     console.log(`New message received: ${message}`);
 
     
@@ -34,7 +62,8 @@ server.on('connection', (ws) => {
 
   
   ws.send('You have connected to the server');
-  
+
+//Disconnect message from WS
   ws.on('close', () => {
       console.log('Client has disconnected from the server')
       clearInterval(interval);
