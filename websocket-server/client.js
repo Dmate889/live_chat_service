85685612e1537a2cf2c9c3d535c6d90d71e49dba
FireWolfy89 <dmate899@gmail.com>
2024-10-09 14:37:10 +0200

    ping-pong healthcheck has been tested

---
+++ websocket-server/client.js
@@ -0,0 +1,23 @@
+//This backend script is dedicated to test the server-side ping-pong WS healthcheck
+
+const WebSocket = require('ws');
+const ws = new WebSocket('ws://localhost:8080');
+
+//Client is connecting to the server
+ws.on('open', () => {
+    console.log('Client has connected to the server');
+
+    ws.send('Hello server!');
+});
+
+//Client sends a message
+ws.on('message', (msg) => {
+    console.log(`Message from the server: ${msg}`);
+});
+
+//Client is 'losing' connection to the server. After this point it should not receive the 'ping', and would not send the 'pong' answer
+setTimeout(() => {
+    console.log('Simulating connection loss to the server');
+    ws.terminate();
+
+}, 10000);
