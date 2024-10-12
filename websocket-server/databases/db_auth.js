f671b01b87ef87997e95b1d312b439edb9bb8c4d
FireWolfy89 <dmate899@gmail.com>
2024-10-12 15:22:32 +0200

    bcrypt pkg implemented for hashing user passwords & JWT implemented

--- websocket-server/databases/db_auth.js
+++ websocket-server/databases/db_auth.js
@@ -1,28 +1,40 @@
 const sqlite3 = require('sqlite3');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = '12345';
+
 
 const db = new sqlite3.Database('./auth.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,  (err) => {
     if(err) console.log('There was an error creating the database');
-    else console.log('The database has been successfully created');
+    else console.log('The database_auth has been successfully created');
 })
 
-db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP)');
+db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, password TEXT, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP)');
 
-function authUsers(id, name){
-    const query = 'INSERT INTO users (id,name,email,createdAt) VALUES (NULL,?,?,?)';
+async function authUsers(name, password){
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = 'INSERT INTO users (name, password, createdAt) VALUES (?,?,?)';
 
-    db.run(query,[id, name], (err) => {
+    db.run(query,[name, hashedPassword], (err) => {
         if(err) console.log('Could not run the query: '+ query);
         else console.log('Data has been inserted to auth.db');
     });
 }
 
-function getUsers(){
-    const query = 'SELECT name FROM users';
+async function getUsers(name, password, callback){
+    const query = 'SELECT name, password FROM users WHERE name = ?';
 
-    db.all(query,[],(err) => {
-        if(err) console.log('Could not run the query: '+ query);
-    })
-
+    db.get(query,[name], async (err, row) => {
+        if(err){
+            console.log('Could not fetch user: ' + err)
+            return callback(err);
+        }
+        if(row && await bcrypt.compare(password, row.password)){
+            const token = jwt.sign({name: row.name}, JWT_SECRET, {expiresIn: '5h'});
+            callback(null,{success: true, token});
+        }
+        else callback(null,{success: false, mesage: 'Invalid username or password'});
+    });
 }
 
 module.exports ={
