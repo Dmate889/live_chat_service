39ea7c5493c21a18bd54ba9ff8ed665e97d06719
FireWolfy89 <dmate899@gmail.com>
2024-10-19 23:39:58 +0200

    Bugfix

--- live_chat_service/src/app/components/register-page/register-page.component.ts
+++ live_chat_service/src/app/components/register-page/register-page.component.ts
@@ -19,12 +19,13 @@ export class RegisterPageComponent {
     const passWord = e.target.elements.password.value.trim();
 
     if(userName === '' || passWord === ''){
-      alert('The usernam or password format was incorrect!');
+      alert('The username or password format is incorrect!');
     }
     else{
       this.authService.registerUser(userName,passWord).subscribe(
         response => {
           console.log('User registered successfully', response);
+          alert(`You have registered successfully as ${userName}`);
           this.router.navigate(['/loginPage']);
         },
         error => {
--- websocket-server/databases/db_auth.js
+++ websocket-server/databases/db_auth.js
@@ -11,30 +11,34 @@ const db = new sqlite3.Database('./auth.db', sqlite3.OPEN_READWRITE | sqlite3.OP
 
 db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, password TEXT, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP)');
 
-async function authUsers(name, password){
+//Inserting new users into the DB
+async function authUsers(name, password){    
     const hashedPassword = await bcrypt.hash(password, 10);
     const query = 'INSERT INTO users (name, password) VALUES (?,?)';
-
+    
     db.run(query,[name, hashedPassword], (err) => {
         if(err) console.log('Could not run the query: '+ query);
         else console.log('Data has been inserted to auth.db');
     });
 }
 
+
+//Getting users from the DB
 async function getUsers(name, password, callback){
     const query = 'SELECT name, password FROM users WHERE name = ?';
-
-    db.get(query,[name], async (err, row) => {
-        if(err){
-            console.log('Could not fetch user: ' + err)
-            return callback(err);
-        }
-        if(row && await bcrypt.compare(password, row.password)){
-            const token = jwt.sign({name: row.name}, JWT_SECRET, {expiresIn: '5h'});
-            callback(null,{success: true, token});
-        }
-         else callback(null,{success: false, message: 'Invalid username or password'});
-    });
+        db.get(query,[name], async (err, row) => {
+            if(err){
+                console.log('Could not fetch user: ' + err)
+                return callback(err);
+            }
+            if(row && await bcrypt.compare(password, row.password)){
+                const token = jwt.sign({name: row.name}, JWT_SECRET, {expiresIn: '5h'});
+                callback(null,{success: true, token});
+            }
+             else callback(null,{success: false, message: 'Invalid username or password'});
+        });
+    
+    
 }
 
 module.exports ={
