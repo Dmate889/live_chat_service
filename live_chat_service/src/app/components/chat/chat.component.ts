868343c76a7e9e05edb4c0b72e5fa85cfa5bef3c
FireWolfy89 <dmate899@gmail.com>
2025-01-01 13:54:18 +0100

    Bugfix for message content, now wrapping words

--- live_chat_service/src/app/components/chat/chat.component.ts
+++ live_chat_service/src/app/components/chat/chat.component.ts
@@ -1,38 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { ChatService } from '../../services/chat.service'; 
+import { ChatService } from '../../services/chat.service';
 import { Router } from '@angular/router';
 import { ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
-
-
-
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit, AfterViewChecked {
   @ViewChild('messageContainer') private messageContainer!: ElementRef;
 
-//The messages are stored in the messages array, coming from the backend and it will be processed in the static HTML
-  messages: {content: string, sender: string, timestamp: Date} [] = []; 
-  newMessage: string = ''; 
+  //The messages are stored in the messages array, coming from the backend and it will be processed in the static HTML
+  messages: { content: string; sender: string; timestamp: Date }[] = [];
+  newMessage: string = '';
   lastMessageTimestamp: number = 0;
   cooldownTime: number = 300;
   showSmileyDropdown = false;
 
-
   constructor(private chatService: ChatService, private router: Router) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.chatService.getMessages().subscribe((message: any) => {
-      
       this.messages.push({
-        content: typeof message.content === 'object' ? JSON.stringify(message.content) : message.content,
+        content:
+          typeof message.content === 'object'
+            ? JSON.stringify(message.content)
+            : message.content,
         sender: message.sender,
-        timestamp: message.timestamp
+        timestamp: message.timestamp,
       });
-      this.newMessage = "";
+      this.newMessage = '';
     });
   }
 
@@ -44,46 +42,52 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     this.showSmileyDropdown = !this.showSmileyDropdown;
   }
 
-
   addSmiley(smiley: string) {
     this.newMessage = (this.newMessage || '') + smiley;
-    this.showSmileyDropdown = false; 
+    this.showSmileyDropdown = false;
   }
 
-//The messages that we send to the WS server 
+  //The messages that we send to the WS server
   sendMessage(event: Event) {
     event.preventDefault();
     const currentTime = Date.now();
-    
-//Spam protection
-  if(currentTime - this.lastMessageTimestamp < this.cooldownTime) return alert('If you keep spamming, you will be disconnected from the server')
 
-//if the message is empty, it can not be sent
-    if(this.newMessage.trim() === '')
-    {
-      alert("You can't send empty messages.");  
+    //Spam protection
+    if (currentTime - this.lastMessageTimestamp < this.cooldownTime)
+      return alert(
+        'If you keep spamming, you will be disconnected from the server'
+      );
+
+    //if the message is empty, it can not be sent
+    if (this.newMessage.trim() === '') {
+      alert("You can't send empty messages.");
       return;
     }
-      const message = {
-        content: this.newMessage,
-        timestamp: currentTime
-      }
+
+    if(this.newMessage.length > 255){
+      alert('You have reached the character limit of 255');
+      return;
+    }
+
+    const message = {
+      content: this.newMessage,
+      timestamp: currentTime,
+    };
 
     this.chatService.sendMessage(JSON.stringify(message));
     this.lastMessageTimestamp = currentTime;
     this.scrollToBottom();
   }
 
-  disconnectUser(e: any){
+  disconnectUser(e: any) {
     this.chatService.closeConnection();
     localStorage.removeItem('jwtToken');
     this.router.navigate(['/loginPage']);
   }
 
-  //This will set the actual scrolling position of the messagecontainer to the scrolling height, which is the last scrolling position, so we will always see the last message withot scrolling
+  //This will set the actual scrolling position of the messagecontainer to the scrolling height, which is the last scrolling position, so we will always see the last message without scrolling
   scrollToBottom(): void {
-    this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
+    this.messageContainer.nativeElement.scrollTop =
+      this.messageContainer.nativeElement.scrollHeight;
   }
 }
-
-
